88fc6c825ee3254af1e5491759f1cb4f093a4eb5
j0taele <jot4ele@gmail.com>
2015-08-26 19:04:43 +0200

    3.8 Lista de videos con Angular.js

--- assets/js/dependencies/video-page.js
+++ assets/js/dependencies/video-page.js
@@ -1,97 +1,116 @@
-$(function whenDomIsReady(){
+angular.module('brushfire_videosPage', [])
+.config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    '*://www.youtube.com/**'
+  ]);
+});
 
+angular.module('brushfire_videosPage').controller('PageCtrl', [
+            '$scope', '$timeout',
+  function ( $scope ,  $timeout ){
 
-  $('.submit-video-form').submit(function (e){
+    /////////////////////////////////////////////////////////////////////////////
+    // Immediately start fetching list of videos from the server.
+    /////////////////////////////////////////////////////////////////////////////
 
-    // Prevent the browser from doing the default form thing, this is 2015.
-    e.preventDefault();
+    // First, show a loading spinner
+    $scope.videosLoading = true;
 
-    // Harvest the data out of the form in the DOM
-    // (this is where you put your client-side validation when relevant)
-    var newVideo = {
-      title: $('.submit-video-form input[name="title"]').val(),
-      src: $('.submit-video-form input[name="src"]').val()
-    };
+    // Then simulate a delay
+    // (TODO: actually fetch videos from server instead of pretending)
+    $timeout(function afterRetrievingVideos (){
 
-    // Clear the form fields
-    $('.submit-video-form input').val('');
-
-    // Now we'll submit the new video to the server:
-
-    // First, show a loading state
-    $('.submit-video-form button').text('Submitting...');
-    // and disable the submit button to prevent double-posting while loading.
-    $('.submit-video-form button').prop('disabled', true);
-
-  // create placeholder anchor element
-    var parser = document.createElement('a');
-
-  // assign url to parser.href
-  parser.href = newVideo.src
-
-  // Use the indexOf parser.search as the first argument and length of
-    // parser.search as the second argument of parser.search.substring
-    // The result is the YouTube ID --> LfOWehvvuo0
-  var youtubeID = parser.search.substring(parser.search.indexOf("=")+1, parser.search.length);
-
-  // Create the source url for the eventual iframe
-    newVideo.src = 'https://www.youtube.com/embed/'+youtubeID;
-
-    // Simulate a delay
-    setTimeout(function (){
       // TODO: handle error state from the server
-      // Success!
-      // Now we know it's the real deal and the server accepted our submission.
 
-      // Build up some HTML for our new video
-      // Note: this approach is just an example-- you should be HTML-escaping things
-      // or better yet, using a more structured front-end web framework on top of (or in lieu of)
-      // jQuery.
-      var newVideoHtml = '<li class="video">'+
-      '  <h2>' + newVideo.title + '</h2>'+
-      '  <iframe width="640" height="390" src="'+newVideo.src+'" frameborder="0" allowfullscreen></iframe>'+
-      '</li>';
+      // Fake data (TODO: use the real data from the server instead of pretending)
+      var _videos = [{
+        title: 'PSY - GANGNAM STYLE (강남스타일) M/V',
+        src: 'https://www.youtube.com/embed/9bZkp7q19f0'
+      }, {
+        title: 'Justin Bieber - Baby ft. Ludacris',
+        src: 'https://www.youtube.com/embed/kffacxfA7G4'
+      }, {
+        title: 'Charlie bit my finger - again !',
+        src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
+      }];
 
+      // Hide the loading spinner:
+      $scope.videosLoading = false;
 
-      // Insert HTML for the newly added video into the DOM
-      $('.the-list-of-videos').prepend(newVideoHtml);
-
-      // Hide the loading state
-      $('.submit-video-form button').text('Submit Video');
-
-      // And re-enable the submit button.
-      $('.submit-video-form button').prop('disabled', false);
+      // Stick the videos into the DOM
+      $scope.videos = _videos;
 
     }, 750);
 
-  });
+    $scope.submitNewVideo = function (){
 
-    
-  $('.the-list-of-videos .loading').show();
+      // A little "spin-lock" to prevent double-submission
+      // (because disabling the submit button still allows double-posts
+      //  if a user hits the ENTER key to submit the form multiple times.)
+      if ($scope.busySubmittingVideo) {
+        return;
+      }
 
-  setTimeout(function afterRetrievingVideos() {
-    var videos = [{
-      title: 'PSY - GANGNAM STYLE (강남스타일) M/V',
-      src: 'https://www.youtube.com/embed/9bZkp7q19f0'
-    }, {
-      title: 'Justin Bieber - Baby ft. Ludacris',
-      src: 'https://www.youtube.com/embed/kffacxfA7G4'
-    }, {
-      title: 'Charlie bit my finger - again !',
-      src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
-    }];
+      // Harvest the data out of the form
+      // (thanks to ng-model, it's already in the $scope object)
+      var _newVideo = {
+        title: $scope.newVideoTitle,
+        src: $scope.newVideoSrc,
+      };
 
-    $('.the-list-of-videos .loading').hide();
+      // create placeholder anchor element
+      var parser = document.createElement('a');
 
-    var videosHtml = _.reduce(videos, function(html, video){
-      html += '<li class="video">' +
-        '  <h2>' + video.title + '</h2>' +
-        '  <iframe width="640" height="390" src="' + video.src + '" frameborder="0" allowfullscreen></iframe>' +
-        '</li>';
-      return html;
-    }, '');
+      // assign url to parser.href
+      parser.href = _newVideo.src
 
-    $('.the-list-of-videos ul').replaceWith(videosHtml);
+      // Use the indexOf parser.search as the first argument and length of
+      // parser.search as the second argument of parser.search.substring
+      // The result is the YouTube ID --> LfOWehvvuo0
+      var youtubeID = parser.search.substring(parser.search.indexOf("=")+1, parser.search.length);
 
-  }, 750); 
-});
+      _newVideo.src = 'https://www.youtube.com/embed/'+youtubeID;
+
+      // (this is where you put your client-side validation when relevant)
+
+      // Side note:
+      // Why not use something like `$scope.videoForm.title` or `$scope.newVideo.title`?
+      // While this certainly keeps things more organized, it is a bit risky in the Angular
+      // world.  I'm no Angular expert, but we have run into plenty of 2-way-binding issues/bugs
+      // in the past from trying to do this.  I've found two guiding principles that help prevent
+      // these sorts of issues:
+      // + very clearly separate the $scope variables in your form from the $scope variables
+      //   representing the rest of your page.
+      // + don't point `ng-model` at the property of an object or array (e.g. `ng-model="foo.bar"`)
+      //   Angular handles its 2-way bindings by reference, and it's not too hard to get into weird
+      //   situations where your objects are all tangled up.
+
+      // Now we'll submit the new video to the server:
+
+      // First, show a loading state
+      // (also disables form submission)
+      $scope.busySubmittingVideo = true;
+
+      // Simulate a delay
+      $timeout(function (){
+        // TODO: handle error state from the server
+        // Success!
+        // Now we know it's the real deal and the server accepted our submission.
+
+        // Insert HTML for the newly added video into the DOM
+        $scope.videos.unshift(_newVideo);
+
+        // Hide the loading state
+        // (also re-enables form submission)
+        $scope.busySubmittingVideo = false;
+
+        // Clear out form inputs
+        $scope.newVideoTitle = '';
+        $scope.newVideoSrc = '';
+
+      }, 750);
+
+    };
+  }
+]);
